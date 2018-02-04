0fe19f342952dd596d131fb152553d8c4080c078
Jones  Evan P <jonesep1@student.cs.appstate.edu>
2018-02-04 16:34:42 -0500

    added the beginnings of the necessary OpenCV code, still need the dependencies to be installed for it to correctly run, will figure that out at some point.

--- cam.java
+++ cam.java
@@ -1,6 +1,7 @@
 package Home;
 
 import JRPiCam.*;
+import org.opencv.*;
 
 public class cam {
     public static void main(String[] args) {
@@ -10,6 +11,13 @@ public class cam {
 	RPiCamera piCam = new RPiCamera("/home/pi/Documents/com/Home/Pictures");
 	piCam.setToDefaults();
 	BufferedImage pic1 = piCam.takeBufferedStill();
-	
+	System.loadLibrary(Core.NATIVE_LIBRARY);
+	while (true)
+	{
+	    Picture a = new Picture(pic1);
+	    Tonemap map = a.createTonemap();
+	    
+	    thread.sleep(1000);
+	}
     }
 }
