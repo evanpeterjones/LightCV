d2698ee336009ed9e88e37c3f6c4985dcc272fac
evanpeterjones <evanpeterjones@gmail.com>
2018-02-05 12:47:03 -0500

    removed opencv dependency, computes active quadrant with proprietary code *winky face*

--- cam.java
+++ cam.java
@@ -1,23 +1,58 @@
 package Home;
 
 import JRPiCam.*;
-import org.opencv.*;
 
 public class cam {
-    public static void main(String[] args) {
+    /** cam() 
+     * 
+     * constructor initiates all variables
+     *
+     */
+    public cam() {
 	light tplight = new light();
 	String newIP = tplight.getIP();
-	System.out.println(newIP);
 	RPiCamera piCam = new RPiCamera("/home/pi/Documents/com/Home/Pictures");
+	System.out.println(newIP);
 	piCam.setToDefaults();
+    }
+    /** takeAndCompare()
+     * 
+     * Takes two photos and creates a double-dimensional array
+     * that discerns the highest area of variance
+     *
+     */
+    public int takeAndCompare() {
 	BufferedImage pic1 = piCam.takeBufferedStill();
-	System.loadLibrary(Core.NATIVE_LIBRARY);
-	while (true)
+	BufferedImage pic2 = piCam.takeBufferedStill();
+	int picCompData[][]= new int[pic2.getWidth()][pic2.getLength()];
+	int width = 0;
+	int height= 0;
+	while (pic2.getHeight() > height)
 	{
-	    Picture a = new Picture(pic1);
-	    Tonemap map = a.createTonemap();
-	    
-	    thread.sleep(1000);
+	    width = 0;
+	    while (pic2.getWidth() > width)
+	    {
+		picCompData[height][width] = Math.abs((pic2.getRGB(height,width)
+						       -pic1.getRGB(height,width)));
+		width++;
+	    }
+	    height++;
 	}
+	return biggestQuadrant(picCompData);
+    }
+
+    public int biggestQuadrant(int[][] data) {
+	
+    }
+
+    /** setLights()
+     *
+     * takes return value from takeAndCompare and sets lights based on 
+     * quadrant of variance
+     */
+    
+    public static void main(String[] args) {
+	cam();
+	
     }
 }
